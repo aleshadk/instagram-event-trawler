41980fb5bd32a9acc642902a2abd8695bc1873f7
aleksey diduh <aleksey.diduh@skyeng.ru>
2025-01-03 16:19:31 -0300

    add simple http route

--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 )
 
+func getPort() string {
+	port := os.Getenv("PORT");
+	if (port != "") {
+		return port
+	}
+
+	return "8080"
+}
+
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+	fmt.Fprintln(w, "Hello, World! " + time.Now().String())
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Server started at http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	port := getPort()
+	fmt.Println("Server started at http://localhost:" + port)
+	http.ListenAndServe(":" + port, nil)
 }
